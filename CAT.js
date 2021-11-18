b3a69dd7342b6e77d7c6b136816f4287f62c9904
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2021-11-18 04:22:59 +0200

    Add files via upload

---
+++ CAT.js
@@ -0,0 +1,8 @@
+let body=document.querySelector('body').style.backgroundColor="ghostwhite";
+let main=document.querySelector("main")
+let squares=document.querySelector(".squares")
+//code
+bg()=>{
+		body.style.backgroundColor="orange";
+	squares.style.backgroundColor="black"
+}
--- app.js
+++ app.js
@@ -5,3 +5,21 @@
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
 })()
+/*
+
+let title=document.getElementById('title'); 
+let main=document.getElementById('images');
+let price=document.getElementById('tag');
+//index.html
+let mtitle=document.getElementById('mtitle'); 
+let spinner=document.getElementById('spinner');
+let mprice=document.getElementById('mprice');
+let body=document.getElementById("body");
+function change(){
+    title.innerHTML=mtitle;
+    price.innerHTML=mprice;
+}
+body.addEventListener("load", function() {
+        
+    spinner.style.display="block";
+}*/
